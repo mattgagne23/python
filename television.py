1ec8a674784e191b9877a501784225d14c1b3a75
Matt Gagne <mgmattgagne23@gmail.com>
2024-11-22 15:37:18 -0600

    Version 2 - add television.py

--- television.py
+++ television.py
@@ -17,7 +17,6 @@ class Television:
             self.__status = True
         elif self.__status == True:
             self.__status = False
-        return self.__status
     
     def mute(self):
         if self.__status == True:
@@ -26,15 +25,11 @@ class Television:
             elif self.__muted == False:
                 self.__muted = True
 
-        return self.__muted
-
     def channel_up(self):
         if self.__status == True:
             self.__channel += 1
             if self.__channel > Television.MAX_CHANNEL:
                 self.__channel = Television.MIN_CHANNEL
-
-        return self.__channel
 
     def channel_down(self):
         if self.__status == True:
@@ -42,15 +37,11 @@ class Television:
             if self.__channel < Television.MIN_CHANNEL:
                 self.__channel = Television.MAX_CHANNEL
 
-        return self.__channel
-
     def volume_up(self):
         if self.__status == True:
             self.__muted == False
             if self.__volume < Television.MAX_VOLUME:
                 self.__volume += 1
-
-        return self.__volume
 
     def volume_down(self):
         if self.__status == True:
@@ -58,8 +49,6 @@ class Television:
             if self.__volume > Television.MIN_VOLUME:
                 self.__volume -= 1
          
-        return self.__volume
-
     def __str__(self):
         if self.__muted == True:
             return f'Power = {self.__status}, Channel = {self.__channel}, Volume = {Television.MIN_VOLUME}'
